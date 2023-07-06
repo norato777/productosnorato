93050f3380478edfaaeedb5333fd208bb9b2d8b7
Edison <norato777@gmail.com>
2023-07-05 21:36:00 -0600

    starting server

---
+++ api/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+// const cors = require("cors");
+// const axios = require("axios");
+// const bodyParser = require("body-parser");
+
+const app = express();
+// require("../db");
+
+// app.use(express.json());
+
+// app.use(cors());
+app.use(morgan("dev"));
+// app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// app.use(bodyParser.json({ limit: "50mb" }));
+
+module.exports = app;
