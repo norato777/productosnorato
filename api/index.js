5ab49ea90836bdc023b0e74f25998e3b22e28e0e
Edison <norato777@gmail.com>
2023-07-09 13:51:07 -0600

    working on it

--- api/index.js
+++ api/index.js
@@ -1,17 +1,20 @@
-// require("dotenv").config();
-// const connection = require("./db");
+require("dotenv").config();
+const connection = require("./db");
 const PORT = process.env.PORT || 3001;
 const app = require("./src/app");
 
-async function main() {
-  await app.listen(PORT, () => {
-    console.log(`server is listening at ${PORT}`);
-  });
-}
+const Users = require("./src/models/User");
 
-main();
+// async function main() {
+//   await app.listen(PORT, () => {
+//     connection();
+//     console.log(`server is listening at ${PORT}`);
+//   });
+// }
 
-// app.listen(PORT, () => {
-//   // connection();
-//   console.log("Server running on port", PORT);
-// });
+// main();
+
+app.listen(PORT, () => {
+  connection();
+  console.log("Server running on port", PORT);
+});
--- api/src/app.js
+++ api/src/app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
-// const cors = require("cors");
-// const axios = require("axios");
-// const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+const bodyParser = require("body-parser");
 
 const app = express();
-// require("../db");
+require("../db");
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.use(cors());
+app.use(cors());
 app.use(morgan("dev"));
-// app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-// app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 module.exports = app;
